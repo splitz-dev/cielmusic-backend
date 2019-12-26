39d53a4a337d5186d5f4eb3de037578c32820830
kang-heewon <itphantom@icloud.com>
2019-12-26 13:00:36 +0900

    feat: ⚡️set index

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ import "./utils/env";
 const app = express();
 console.log(`Current NODE_ENV is ${process.env.NODE_ENV}`);
 
+app.get("/", (_, res) => {
+  res.send("hello");
+});
+
 useExpressServer(app, {
   cors: true,
   controllers: [`${__dirname}/controllers/*.[jt]s`],
