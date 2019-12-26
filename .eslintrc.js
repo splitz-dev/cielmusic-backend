3ee7bf2a89213d54ff56766aeda6630df204c9dc
kang-heewon <itphantom@icloud.com>
2019-12-26 13:00:22 +0900

    chore: 📦 set eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,14 @@ module.exports = {
     // Airbnb style guide 적용
     "airbnb-base",
     // TypeScript ESLint recommanded style 적용
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:prettier/recommended",
-    "eslint-plugin-prettier"
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended"
   ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
   rules: {
     "prettier:prettier": [
       "error",
