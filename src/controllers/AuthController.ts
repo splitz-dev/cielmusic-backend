aa86cea8059c8e464557058d2d16695541e5a856
kang-heewon <itphantom@icloud.com>
2020-01-09 00:40:18 +0900

    fix: 🔥rename

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,52 +5,50 @@ import {
   CurrentUser,
   Get,
   JsonController,
-  Param,
-  Post
-} from "routing-controllers";
-import { User } from "../models/User";
-import { UserService } from "../services/UserService";
-import { UserCreateDto } from "../Dto/UserCreateDto";
+  Post,
+} from 'routing-controllers';
+import { User } from '../models/User';
+import { UserService } from '../services/UserService';
+import { UserCreateDto } from '../Dto/UserCreateDto';
 
 @JsonController()
 export class AuthController {
-  @Post("/sign-in")
+  @Post('/login')
   async signIn(
-    @BodyParam("email", { required: true, validate: true }) email: string,
-    @BodyParam("password", { required: true, validate: true }) password: string
+    @BodyParam('email', { required: true, validate: true }) email: string,
+    @BodyParam('password', { required: true, validate: true }) password: string
   ) {
     const user = await new UserService().signIn(email, password);
     return {
       ...user,
-      password: undefined
+      password: undefined,
     };
   }
 
-  @Post("/sign-up")
+  @Post('/register')
   async signUp(
     @Body({ required: true, validate: true }) userForm: UserCreateDto
   ) {
     const user = await new UserService().signUp(userForm);
     return {
       ...user,
-      password: undefined
+      password: undefined,
     };
   }
 
   @Authorized()
-  @Get("/sign-out")
+  @Get('/logout')
   signOut() {
     return {
-      result: `sign out successfully`
+      result: `sign out successfully`,
     };
   }
 
   // 토큰으로 내 정보 호출
-  @Get("/user-by-token/:token")
-  getUserByToken(
-    @CurrentUser({ required: true }) _: User,
-    @Param("token") token: string
-  ) {
-    return new UserService().getUserByToken(token);
+  @Get('/myinfo')
+  getUser(@CurrentUser({ required: true }) user: User) {
+    return {
+      user,
+    };
   }
 }
