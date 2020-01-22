5821d3d51a12bc3631dbb585e968af76c38548b3
kang-heewon <itphantom@icloud.com>
2020-01-23 01:14:41 +0900

    feat:⚡️search controller

---
+++ src/controllers/SearchController.ts
@@ -0,0 +1,44 @@
+import { Get, Post, BodyParam, JsonController } from 'routing-controllers';
+import { SearchService } from '../services/SearchService';
+@JsonController('/search')
+export class SearchController {
+  @Get('/music')
+  async searchMusic(
+    @BodyParam('query') query: string,
+    @BodyParam('take') take: number,
+    @BodyParam('skip') skip: number
+  ) {
+    const music = await new SearchService().getSongByTitle(query, take, skip);
+    return {
+      result: music,
+    };
+  }
+
+  @Get('/album')
+  async searchAlbum(
+    @BodyParam('query') query: string,
+    @BodyParam('take') take: number,
+    @BodyParam('skip') skip: number
+  ) {
+    const album = await new SearchService().getAlbumByTitle(query, take, skip);
+    return {
+      result: album,
+    };
+  }
+
+  @Get('/artist')
+  async searchArtist(
+    @BodyParam('query') query: string,
+    @BodyParam('take') take: number,
+    @BodyParam('skip') skip: number
+  ) {
+    const artist = await new SearchService().getArtistByTitle(
+      query,
+      take,
+      skip
+    );
+    return {
+      result: artist,
+    };
+  }
+}
